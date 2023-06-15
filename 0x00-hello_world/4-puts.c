d266420dda76b9d2645bc15e7c9807465f24f51a
Wahba <ahmd.wahba@outlook.comgit config --global user.name AhmdWahba~>
2023-06-15 22:55:03 +0000

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,12 @@
 /**
  * main - Entry point
  *
- * description: A C program that print with puts function 
- *
  * Return: Always 0 (Success)
-*/
- 
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
+
