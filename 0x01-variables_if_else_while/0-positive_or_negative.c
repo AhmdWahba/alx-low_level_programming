4d5c7254f3f07d5c96d6924475aa5f48b873e1aa
Wahba <ahmd.wahba@outlook.comgit config --global user.name AhmdWahba~>
2023-06-16 20:03:50 +0000

    If..else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/*
+ * */
+
+int main(void)
+{
+
+	int num=%d;
+
+	if(int num > 0)
+{
+	printf("is positive");
+}
+	
+	if(int num = 0)
+{
+	printf("is zero");
+}
+	
+	if(int < 0)
+{
+	printf("is negative");
+}
+}
