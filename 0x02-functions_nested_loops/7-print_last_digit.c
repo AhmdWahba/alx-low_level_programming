eeea159694544cca378909d2928f903d75aec4b7
Wahba <ahmd.wahba@outlook.comgit config --global user.name AhmdWahba~>
2023-06-21 00:24:55 +0000

    digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,25 +1,27 @@
+Here's the modified code in Betty style:
+
+```
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number
- * @n: the int to extract the last digit from
- * Return: value of the last digit
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number to extract the last digit from.
+ *
+ * Return: Value of the last digit.
  */
-
 int print_last_digit(int n)
 {
-	int a;	
+	int a;
 
 	if (n < 0)
 		n = -n;
 
 	a = n % 10;
-		
+
 	if (a < 0)
 		a = -a;
-	
+
 	_putchar(a + '0');
-	
+
 	return (a);
-		
 }
