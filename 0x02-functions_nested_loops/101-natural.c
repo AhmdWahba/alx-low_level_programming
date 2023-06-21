14a471f5fbeace07e9d37d244ac0ae1a73dfbc0b
Wahba <ahmd.wahba@outlook.comgit config --global user.name AhmdWahba~>
2023-06-21 00:57:31 +0000

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,6 @@
 /*
  * File: 101-natural.c
- * Auth: vincent kip
+ * Auth: Vincent Kip
  */
 
 #include <stdio.h>
@@ -13,18 +13,15 @@
  */
 int main(void)
 {
-	int i, sum = 0;
+    int i, sum = 0;
 
+    for (i = 0; i < 1024; i++)
+    {
+        if ((i % 3) == 0 || (i % 5) == 0)
+            sum += i;
+    }
 
-	for (i = 0; i < 1024; i++)
-	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-			sum += i;
-	}
+    printf("%d\n", sum);
 
-
-	printf("%d\n", sum);
-		
-
-	return (0);
+    return (0);
 }
