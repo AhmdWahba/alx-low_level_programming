6d05e4a95ed791a3b4ec90cc2c751221ab137be0
Wahba <ahmd.wahba@outlook.comgit config --global user.name AhmdWahba~>
2023-06-21 01:27:40 +0000

    .

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,33 +1,31 @@
 /*
- * File: 0x02-conditional_loops/101-natural.c
- * Author: Your Name
- * Date: 2023-06-21
- * Description: Computes and prints the sum of all the multiples of 3 or 5
- *              below 1024 (excluded).
+ * File: 101-natural.c
+ * Auth: vincent kip
  */
 
 #include <stdio.h>
 
 /**
- * main - Computes and prints the sum of all the multiples of 3 or 5
- *        below 1024 (excluded).
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-    int i, sum = 0;
+	int i, sum = 0;
 
-    for (i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
 
-    printf("%d\n", sum);
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
 
-    return 0;
+
+	printf("%d\n", sum);
+
+
+	return (0);
 }
