344deec06e270876d2e275635bfe56ca3f02ecaf
AhmdWahba <ahmd.wahba@outlook.comgit config --global user.name AhmdWahba>
2023-10-03 05:39:29 +0300

    libmy.a

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -5,13 +5,14 @@
  * @dest: input value
  * @src: input value
  *
- * Return: void
+ * Return: void	
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
+
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -24,7 +25,8 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}
+	}		
+
 
 	dest[i] = '\0';
 	return (dest);
--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -11,6 +11,7 @@ char *_strpbrk(char *s, char *accept)
 {
 	int k;
 
+
 	while (*s)
 	{
 		for (k = 0; accept[k]; k++)
@@ -20,6 +21,7 @@ char *_strpbrk(char *s, char *accept)
 		}
 	s++;
 	}
+
 
 return ('\0');
 }
